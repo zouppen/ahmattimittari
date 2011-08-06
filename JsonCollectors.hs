3807c32a920acc8a5d0e77f57efd2e516e1adad0
Joel Lehtonen <joel.lehtonen@iki.fi>
2011-08-06 20:53:55 +0300

    Measurement encoding to JSON works.

--- JsonCollectors.hs
+++ JsonCollectors.hs
@@ -1,11 +1,14 @@
 module JsonCollectors where
 
+import Text.JSON
 import ConfigReader
 import Parsing (perhapsParseFile)
 
 -- measure :: FilePath -> 
 measure conffile = do
   confs <- readConfigFromFile conffile 
-  mapM doParse confs
-  where doParse a = perhapsParseFile (parser a) (device a)
-  
+  list <- mapM doParse confs
+  return $ makeObj list
+  where doParse a = do
+          value <- perhapsParseFile (parser a) (device a) 
+          return (name a,showJSON value)
