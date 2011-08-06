49209dc68f940c37b1932e534297fdd93506c4fc
Joel Lehtonen <joel.lehtonen@iki.fi>
2011-08-07 00:20:32 +0300

    Preliminary database writer is finished.

--- JsonCollectors.hs
+++ JsonCollectors.hs
@@ -4,15 +4,29 @@ import Data.Either
 import Text.JSON
 import ConfigReader
 import Parsing (perhapsParseFile)
+import Database.CouchDB
 
-measure :: FilePath -> IO JSValue
-measure conffile = do
-  confs <- readConfigFromFile conffile 
-  collected <- mapM doParse confs
+type Entry = (String, JSValue)
+
+-- |Performs a measurement and writes the results to local CouchDB. Sweet!
+measureToDatabase :: FilePath -> IO ()
+measureToDatabase conffile = do
+  conf <- readConfigFromFile conffile 
+  json <- measure conf
+  (doc,_) <- runCouchDB' $ newDoc (db "measurements") json
+  putStrLn $ "Measurement ready, id=" ++ show doc
+
+-- |Performs measurement by the instructions in config file
+measure :: [Collector] -> IO JSValue
+measure collectors = do
+  collected <- mapM doParse collectors
   -- Collects errors to a separate object to ease fetching.
   let errors = ("errors",makeObj (lefts collected))
   return $ makeObj (errors:rights collected)
 
+-- |Collects a single measurement and puts successful ones to right
+-- and errors to left.
+doParse :: Collector -> IO (Either Entry Entry)
 doParse a = do
   catch act err
   where
