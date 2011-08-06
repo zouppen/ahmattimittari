03d214d6b912e9b5c237f656487ce071be3f1b97
Joel Lehtonen <joel.lehtonen@iki.fi>
2011-08-06 23:58:43 +0300

    "measure" places errors in handy object.

--- JsonCollectors.hs
+++ JsonCollectors.hs
@@ -1,14 +1,22 @@
 module JsonCollectors where
 
+import Data.Either
 import Text.JSON
 import ConfigReader
 import Parsing (perhapsParseFile)
 
--- measure :: FilePath -> 
+measure :: FilePath -> IO JSValue
 measure conffile = do
   confs <- readConfigFromFile conffile 
-  list <- mapM doParse confs
-  return $ makeObj list
-  where doParse a = do
-          value <- perhapsParseFile (parser a) (device a) 
-          return (name a,showJSON value)
+  collected <- mapM doParse confs
+  -- Collects errors to a separate object to ease fetching.
+  let errors = ("errors",makeObj (lefts collected))
+  return $ makeObj (errors:rights collected)
+
+doParse a = do
+  catch act err
+  where
+    act = do
+      value <- perhapsParseFile (parser a) (device a)
+      return $ Right (name a, showJSON value)
+    err x = return $ Left (name a, showJSON $ show x)
