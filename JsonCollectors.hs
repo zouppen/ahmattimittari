defea4ab892fdcf83349e0ba009e444ffd46ddf4
Joel Lehtonen <joel.lehtonen@iki.fi>
2011-08-07 14:14:22 +0300

    Appends timestamps to database records.

--- JsonCollectors.hs
+++ JsonCollectors.hs
@@ -5,6 +5,8 @@ import Text.JSON
 import ConfigReader
 import Parsing (perhapsParseFile)
 import Database.CouchDB
+import System.Posix.Time (epochTime)
+import Data.Ratio (numerator)
 
 type Entry = (String, JSValue)
 
@@ -20,9 +22,10 @@ measureToDatabase conffile = do
 measure :: [Collector] -> IO JSValue
 measure collectors = do
   collected <- mapM doParse collectors
+  time <- jsEpoch
   -- Collects errors to a separate object to ease fetching.
   let errors = ("errors",makeObj (lefts collected))
-  return $ makeObj (errors:rights collected)
+  return $ makeObj (("timestamp",time):errors:rights collected)
 
 -- |Collects a single measurement and puts successful ones to right
 -- and errors to left.
@@ -34,3 +37,11 @@ doParse a = do
       value <- perhapsParseFile (parser a) (device a)
       return $ Right (name a, showJSON value)
     err x = return $ Left (name a, showJSON $ show x)
+
+-- |Produces UNIX timestamp as JSON value.
+jsEpoch :: IO JSValue
+jsEpoch = do
+  raw <- epochTime
+  -- Numerator makes the assumption that the denominator is one. Not very safe.
+  let unix = numerator $ realToFrac raw
+  return $ showJSON (unix::Integer)
